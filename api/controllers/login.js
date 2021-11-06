0585f11548458d29de7d8e457ffe08b81192c72f
Vishal Kumar <43844177+vishalkss@users.noreply.github.com>
2021-11-07 00:40:55 +0530

    verifyToken api response changes

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -120,7 +120,7 @@ const verifyToken = async (req, res) => {
         await User.findById(req.userId, { password: 0 }, function (err, user) {
             if (err) return res.status(500).send("There was a problem finding the user.");
             if (!user) return res.status(404).send("No user found.");
-            res.status(200).json({ user })
+            res.status(200).json({ token: token, user: user })
         });
     });
 
